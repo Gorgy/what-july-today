38269f977fac0bfae6d05a3316c8a5dcdd264cd2
Belov Alexey <belovaa@lad24.ru>
2023-10-23 21:03:35 +0300

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import 'dayjs/locale/ru'
+import July from './components/July.vue'
+</script>
+
+<template>
+  <July />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/July.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+  import * as dayjs from "dayjs";
+
+  const todayMonth = dayjs().get('M');
+  const year = dayjs().get('year');
+  const difference =
+      todayMonth < 6 ?
+      dayjs(`07.01.${year-1}`).diff(dayjs(), 'd') :
+      dayjs().diff(dayjs(`07.01.${year}`), 'd')
+
+  const date = ref(`${todayMonth === 6 ? dayjs().format('dd') : difference} и Юля`)
+</script>
+
+<template>
+  <h1>{{ date }}</h1>
+</template>
+
+<style scoped>
+
+</style>
